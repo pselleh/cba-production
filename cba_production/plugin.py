cfd7c6a18c29c31af8bcedfc3e6c44c4c38a07da
Phil Selleh <pselleh@gmail.com>
2026-07-03 14:04:57 +0000

    Refactor production deployment plugin
    - Move production security settings into cba-production
    - Remove tracked Python bytecode
    - Ignore Python cache files

--- cba_production/plugin.py
+++ cba_production/plugin.py
@@ -1,21 +1,52 @@
 from tutor import hooks
 from . import security
 
+CBA_COOKIE_DOMAIN = ".centerforbusinessacceleration.com"
+
 #
 # LMS Production Settings
 #
 hooks.Filters.ENV_PATCHES.add_item((
     "openedx-lms-common-settings",
-    """
+    f"""
 DEBUG = False
+
+SESSION_COOKIE_DOMAIN = "{CBA_COOKIE_DOMAIN}"
+CSRF_COOKIE_DOMAIN = "{CBA_COOKIE_DOMAIN}"
 
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
 SESSION_COOKIE_SAMESITE = "None"
 CSRF_COOKIE_SAMESITE = "None"
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+CSRF_TRUSTED_ORIGINS = list(dict.fromkeys(
+    list(globals().get("CSRF_TRUSTED_ORIGINS", [])) + [
+        "https://learn.centerforbusinessacceleration.com",
+        "https://studio.centerforbusinessacceleration.com",
+        "https://apps.learn.centerforbusinessacceleration.com",
+        "https://centerforbusinessacceleration.com",
+        "https://www.centerforbusinessacceleration.com",
+    ]
+))
+
+CORS_ORIGIN_WHITELIST = list(dict.fromkeys(
+    list(globals().get("CORS_ORIGIN_WHITELIST", [])) + [
+        "https://apps.learn.centerforbusinessacceleration.com",
+        "https://centerforbusinessacceleration.com",
+        "https://www.centerforbusinessacceleration.com",
+    ]
+))
+
+LOGIN_REDIRECT_WHITELIST = list(dict.fromkeys(
+    list(globals().get("LOGIN_REDIRECT_WHITELIST", [])) + [
+        "apps.learn.centerforbusinessacceleration.com",
+        "centerforbusinessacceleration.com",
+        "www.centerforbusinessacceleration.com",
+    ]
+))
 """
 ))
 
@@ -24,7 +55,15 @@ CSRF_COOKIE_SAMESITE = "None"
 #
 hooks.Filters.ENV_PATCHES.add_item((
     "openedx-cms-common-settings",
-    """
+    f"""
 DEBUG = False
+
+SESSION_COOKIE_DOMAIN = "{CBA_COOKIE_DOMAIN}"
+CSRF_COOKIE_DOMAIN = "{CBA_COOKIE_DOMAIN}"
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 """
 ))
